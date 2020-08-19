f3a6a44ab8505e61be328bb0cfd855f0129cf2fc
Ivar Jönsson <36239671+ChunkTreasure1@users.noreply.github.com>
2020-08-19 13:40:45 +0200

    Update

--- Lamp/src/Lamp/Event/Event.h
+++ Lamp/src/Lamp/Event/Event.h
@@ -4,7 +4,7 @@
 
 namespace Lamp
 {
-	// Events in Cactus are currently blocking, meaning when an event occurs it
+	// Events in Lamp are currently blocking, meaning when an event occurs it
 	// immediately gets dispatched and must be dealt with right then an there.
 	// For the future, a better strategy might be to buffer events in an event
 	// bus and process them during the "event" part of the update stage.
