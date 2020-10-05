bb1d0c40d7ba5d00648e5d04850a153eda03749b
Ivar Jönsson <36239671+ChunkTreasure1@users.noreply.github.com>
2020-10-05 11:26:16 +0200

    Update

--- Lamp/src/Platform/Direct3D11/Direct3D11RendererAPI.cpp
+++ Lamp/src/Platform/Direct3D11/Direct3D11RendererAPI.cpp
@@ -15,6 +15,20 @@ namespace Lamp
 
 	void Direct3D11RendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
 	{
+		if (WindowsWindow* pWindow = static_cast<WindowsWindow*>(&Application::Get().GetWindow()))
+		{
+			if (Direct3D11Context* pContext = static_cast<Direct3D11Context*>(pWindow->GetGraphicsContext().get()))
+			{
+				//D3D11_VIEWPORT vp;
+				//vp.Width = width;
+				//vp.Height = height;
+				//vp.MinDepth = 0;
+				//vp.MaxDepth = 1;
+				//vp.TopLeftX = x;
+				//vp.TopLeftY = y;
+				//pContext->GetDeviceContext()->RSSetViewports(1u, &vp);
+			}
+		}
 	}
 
 	void Direct3D11RendererAPI::SetClearColor(const glm::vec4& color)
@@ -50,7 +64,7 @@ namespace Lamp
 				const Vertex vertices[] =
 				{
 					{ 0.f, 0.5f },
-					{ 0.5f, 0.5f },
+					{ 0.5f, -0.5f },
 					{ -0.5f, -0.5f }
 				};
 
@@ -74,21 +88,43 @@ namespace Lamp
 				const uint32_t offset = 0u;
 
 				//Bind the vertex buffer
-				pContext->GetDeviceContext()->IASetVertexBuffers(0u, 1u, &pVertexBuffer, &stride, &offset);
+				pContext->GetDeviceContext()->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
 
 				wrl::ComPtr<ID3D11VertexShader> pVS;
 				wrl::ComPtr<ID3D11PixelShader> pPS;
 				wrl::ComPtr<ID3DBlob> pBlob;
 
+				D3DReadFileToBlob(L"PixelShader.cso", &pBlob);
+				pContext->GetDevice()->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPS);
+				pContext->GetDeviceContext()->PSSetShader(pPS.Get(), nullptr, 0u);
+
 				D3DReadFileToBlob(L"VertexShader.cso", &pBlob);
 				pContext->GetDevice()->CreateVertexShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pVS);
 				pContext->GetDeviceContext()->VSSetShader(pVS.Get(), nullptr, 0u);
 
-				pContext->GetDeviceContext()->Draw((uint32_t)std::size(vertices), 0u);
+				//Create input layout
+				wrl::ComPtr<ID3D11InputLayout> pInputLayout;
+				const D3D11_INPUT_ELEMENT_DESC ied[] =
+				{
+					{ "POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+				};
 
-				D3DReadFileToBlob(L"PixelShader.cso", &pBlob);
-				pContext->GetDevice()->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPS);
-				pContext->GetDeviceContext()->PSSetShader(pPS.Get(), nullptr, 0u);
+				pContext->GetDevice()->CreateInputLayout(ied, (uint32_t)std::size(ied), pBlob->GetBufferPointer(), pBlob->GetBufferSize(), &pInputLayout);
+				pContext->GetDeviceContext()->IASetInputLayout(pInputLayout.Get());
+
+				pContext->GetDeviceContext()->OMSetRenderTargets(1u, pContext->GetRenderTarget().GetAddressOf(), nullptr);
+				pContext->GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+				D3D11_VIEWPORT vp;
+				vp.Width = 1280;
+				vp.Height = 720;
+				vp.MinDepth = 0;
+				vp.MaxDepth = 1;
+				vp.TopLeftX = 0;
+				vp.TopLeftY = 0;
+				pContext->GetDeviceContext()->RSSetViewports(1u, &vp);
+
+				pContext->GetDeviceContext()->Draw((uint32_t)std::size(vertices), 0u);
 			}
 		}
 	}
