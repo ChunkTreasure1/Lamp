8173a1fa5bc10db58bd427126c8323042ba17eb1
Ivar Jönsson <36239671+ChunkTreasure1@users.noreply.github.com>
2021-10-26 18:05:17 +0200

    Added SSS

---
+++ Sandbox/engine/shaders/main/screenSpaceSSS.glsl
@@ -0,0 +1,18 @@
+#ShaderSpec
+Name: screenSpaceSSS
+TextureCount: 0
+TextureNames
+{
+}
+
+#type vertex
+#version 440 core
+layout (location = 0) in vec3 a_Position;
+layout (location = 1) in vec3 a_Normal;
+layout (location = 2) in vec3 a_Tangent;
+
+void main()
+{}
+
+#type fragment
+#version 440 core
